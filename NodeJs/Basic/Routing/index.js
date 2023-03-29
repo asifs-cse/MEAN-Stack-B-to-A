8b3c6514119e5c1059dfc18f006f2c01e5e5b6dd
unknown <asifs.cse@gmail.com>
2023-03-30 00:02:10 +0530

    create route for html fiel

--- NodeJs/Basic/Routing/index.js
+++ NodeJs/Basic/Routing/index.js
@@ -1,9 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
 const hostName = '127.0.0.1';
 const server = http.createServer((req, res)=>{
-    res.end("Welcome to server!");
+    if(req.url=='/'){
+        fs.readFile("./views/home.html",(err,data)=>{
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }else if(req.url=='/about'){
+        fs.readFile("./views/about.html",(err,data)=>{
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }else if(req.url=='/contact'){
+        fs.readFile("./views/contact.html",(err,data)=>{
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }else{
+        fs.readFile("./views/error.html",(err,data)=>{
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
 })
 
 server.listen(port, hostName,()=>{
