6b71dbed639bdf1ba5e297bbff7aaf8203312c5d
unknown <asifs.cse@gmail.com>
2023-03-29 22:12:24 +0530

    using path module

--- NodeJs/Basic/OSModule/index.js
+++ NodeJs/Basic/OSModule/index.js
@@ -1,7 +1,21 @@
 //os path
-const os = require('os');
-console.log(os.userInfo());
-console.log(os.homedir());
-console.log(os.hostname());
+// const os = require('os');
+// console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.__dirname());
 
-console.log(os.totalmem());
+// console.log(os.totalmem());
+
+
+//path
+
+const path = require('path');
+
+//console.log(path);
+
+const extensionName = path.extname("index.html");
+console.log(extensionName);
+
+const joinName = path.join(__dirname + "/views");
+console.log(joinName);
