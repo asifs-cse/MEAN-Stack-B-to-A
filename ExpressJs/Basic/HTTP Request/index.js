c06cf4261e2ef8b631088af01749d73581d5ab8d
unknown <asifs.cse@gmail.com>
2023-04-01 21:27:59 +0530

    get data using header

--- ExpressJs/Basic/HTTP Request/index.js
+++ ExpressJs/Basic/HTTP Request/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+//get value using query
 app.get('/', (req, res)=>{
     const id= req.query.id; //get value using quresy
     res.send(`<h1>Student id is :${id}</h1>`);
 });
+
+//get value using routes params
 
 app.get('/userId/:id/userAge/:age', (req, res)=>{
     const id = req.params.id; //get value in route params
@@ -13,6 +16,17 @@ app.get('/userId/:id/userAge/:age', (req, res)=>{
 
     res.send(`<h1>User id is: ${id}, user age is: ${age}</h1>`);
 });
+
+app.get('/header', (req, res)=>{
+    const id = req.header('id'); //get value in route params
+    const age = req.header('age');
+
+    res.send(`<h1>User id is: ${id}, user age is: ${age}</h1>`);
+});
+
+//get value using header
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server open at http://localhost:${PORT}`);
