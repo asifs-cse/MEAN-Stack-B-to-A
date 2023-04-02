df03f184280b53a6a6c61bfff9bb2b141862ac58
unknown <asifs.cse@gmail.com>
2023-04-02 10:55:53 +0530

    get value using reguler expression

---
+++ ExpressJs/Basic/Regulr expressin/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 4040;
+
+app.get('/products/:id([0-9]+)', (req, res)=>{ //get o to 9any intiger number
+    res.send(`<h1>ID = ${req.params.id}</h1>`);
+});
+
+app.get('*', (req, res)=>{
+    res.status(404).send({
+        message: "not a valid route",
+    });
+});
+
+app.listen(PORT, (req, res)=>{
+    console.log(`Server is open at http://localhost:${PORT}`);
+});
