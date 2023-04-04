7a7e93fda01c75c99ab3b9dc3d1f754feab54228
unknown <asifs.cse@gmail.com>
2023-04-04 23:08:06 +0530

    stroe data

--- MongoDB/Basic/Connection/index.js
+++ MongoDB/Basic/Connection/index.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3002;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 // mongoose.connect("mongodb://localhost:27017/testProductBD")
 // .then(()=>console.log("db is connected"))
 // .catch((err)=>{
@@ -17,11 +20,7 @@ const PORT = 3002;
 const productSchema = new mongoose.Schema({
     title: String,
     price: Number,
-    description: String,
-    createAd: {
-        type: Date,
-        default: Date.now
-    }
+    description: String
 });
 //create product model
 
@@ -29,7 +28,7 @@ const product = mongoose.model("Products", productSchema);
 
 const dbConnect = async ()=>{
     try{
-        mongoose.connect("mongodb://localhost:27017/testProductBD");
+        mongoose.connect("mongodb://127.0.0.1:27017/testProductBD");
         console.log("db is connect");
     }catch(error){
         console.log("db is not connect");
@@ -42,6 +41,26 @@ const dbConnect = async ()=>{
 
 app.get('/',(req, res)=>{
     res.send('Welcome to home page');
+});
+
+app.post('/products', async(req, res)=>{
+    try{
+        const title= req.body.title;
+        const price= req.body.price;
+        const description = req.body.description;
+
+        const newProduct = new product({
+            title: title,
+            price: price,
+            description: description,
+        });
+
+        const productData = await newProduct.save();
+        res.send(productData);
+        console.log(title);
+    }catch(error){
+        res.status(500).send({message: error});
+    }
 });
 
 app.listen(PORT, async ()=>{
