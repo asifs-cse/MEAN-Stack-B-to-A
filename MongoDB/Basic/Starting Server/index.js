0e22569d6211d98c4de545edb370dcc03d8f528b
unknown <asifs.cse@gmail.com>
2023-04-06 15:14:59 +0530

    connection successfully

--- MongoDB/Basic/Starting Server/index.js
+++ MongoDB/Basic/Starting Server/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = 4000;
 
-app.listen(PORT, (req, res)=>{
+const mongoDB = async ()=>{
+    try {
+        mongoose.connect("mongodb://127.0.0.1:27017/MEANTestDB");
+        console.log('db is connecte...');
+    } catch (error) {
+        console.log('db is not connect...');
+        console.log(error.message());
+        process.exit(1);
+    }
+}
+
+app.listen(PORT,async (req, res)=>{
     console.log(`Server is open at http://localhost${PORT}`);
+    await mongoDB();
 });
