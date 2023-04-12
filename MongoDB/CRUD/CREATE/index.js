dec9627e62e2654a53ffc8e94958b4bae7da68b8
unknown <asifs.cse@gmail.com>
2023-04-12 23:18:33 +0530

    create mongo db connection

--- MongoDB/CRUD/CREATE/index.js
+++ MongoDB/CRUD/CREATE/index.js
@@ -1,7 +1,22 @@
 const expresss = require('express');
+const mongoose = require('mongoose');
 const app = expresss();
 const PORT = 3002;
 
-app.listen(PORT, ()=>{
+//connect mongodb
+const dbConnect = async ()=>{
+    try {
+        await mongoose.connect("mongodb://localhost:27017/acoe");
+        console.log('db is connected');
+    } catch (error) {
+        console.log('db is not connected');
+        console.log(error.message);
+        process.exit(1);
+    }
+};
+
+
+app.listen(PORT, async ()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+    await dbConnect();
+});
