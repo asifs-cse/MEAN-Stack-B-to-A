e2e40df1f513b88f49514b90157b3853905b9610
unknown <asifs.cse@gmail.com>
2023-04-13 10:34:17 +0530

    db connect in server

--- MongoDB/CRUD/UPDATE/index.js
+++ MongoDB/CRUD/UPDATE/index.js
@@ -3,6 +3,22 @@ const mongooe = require('mongoose');
 const app = express();
 const PORT = 3031;
 
+//connect mongodb in express server
+const dbConnect = async ()=>{
+    try {
+        await mongooe.connect("mongodb://127.0.0.1:27017/acoe");
+        console.log('Database onnection successfully!');
+    } catch (error) {
+        console.log('Database conection failed');
+        console.log(error.message());
+        process.exit(1);
+    }
+    
+}
+
+//create express server
 app.listen(PORT, ()=>{
     console.log(`Server is open at http://localhost${PORT}`);
+    //call db function
+    dbConnect();
 })
