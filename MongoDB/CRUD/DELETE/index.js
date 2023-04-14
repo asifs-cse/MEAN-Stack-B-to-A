fded3dd74e3346a445f4be352a1001f813e6c47e
unknown <asifs.cse@gmail.com>
2023-04-14 10:05:29 +0530

    database conect

--- MongoDB/CRUD/DELETE/index.js
+++ MongoDB/CRUD/DELETE/index.js
@@ -4,7 +4,22 @@ const PORT = 3032;
 const app = express();
 
 
+//connect database
+const dbConnect = ()=>{
+    try {
+        mongoose.connect("mongodb://127.0.0.1:27017/acoe");
+        console.log('database was connected successfully');
+    } catch (error) {
+        console.log(error);
+        console.log('database not connected');
+        process.exit(1);
+    }
+};
+
+
+
 //create express server
 app.listen(PORT, (req, res)=>{
     console.log(`Server is open at http://localhost:${PORT}`);
+    dbConnect();
 })
