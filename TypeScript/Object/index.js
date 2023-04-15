12750c00f5f23674e9fdf9713e1dc9814a76d18c
unknown <asifs.cse@gmail.com>
2023-04-15 16:53:51 +0530

    objects

---
+++ TypeScript/Object/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const user = {
+    username: 'asif shahriar',
+    password: 12345
+};
+console.log(user.username);
+console.log(user.password);
---
+++ TypeScript/Object/index.ts
@@ -0,0 +1,11 @@
+//object
+interface user{
+    username: string, 
+    password: number
+}
+const user : user = {
+    username: 'asif shahriar',
+    password: 12345
+};
+console.log(user.username);
+console.log(user.password);
