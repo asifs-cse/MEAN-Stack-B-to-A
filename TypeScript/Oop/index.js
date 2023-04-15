2cc1f8a4e95711276eeddcc1dbddddac14d0659d
unknown <asifs.cse@gmail.com>
2023-04-15 21:31:07 +0530

    generic function

--- TypeScript/Oop/index.js
+++ TypeScript/Oop/index.js
@@ -4,13 +4,31 @@ class P {
     constructor(a) {
         this.x = a;
     }
+    say() {
+        console.log('say parents');
+    }
 }
+//inherite
 class C extends P {
     constructor(a, b) {
         super(a);
         this.y = b;
     }
+    //overright
+    say() {
+        console.log('say chid');
+    }
 }
 let obj = new P("hello");
 let obj2 = new C("HELLO", "world");
-console.log(obj2.x, obj2.y);
+console.log(obj2.say());
+//non generics
+// function identity(arg: any):any{
+//     return arg;
+// }
+//generic
+function identity(arg) {
+    return arg;
+}
+console.log(identity(25));
+console.log(identity('Hello'));
--- TypeScript/Oop/index.ts
+++ TypeScript/Oop/index.ts
@@ -4,17 +4,39 @@ class P{
     constructor(a: string){
         this.x = a;
     }
+    say(){
+        console.log('say parents');
+    }
 }
 
+//inherite
 class C extends P{
     y:string;
     constructor(a:string, b:string){
         super(a);
         this.y = b;
     }
+    //overright
+    say(){
+        console.log('say chid');
+    }
 }
 
 
 let obj = new P("hello");
 let obj2 = new C("HELLO","world");
-console.log(obj2.x, obj2.y);
+console.log(obj2.say());
+
+
+//non generics
+
+// function identity(arg: any):any{
+//     return arg;
+// }
+//generic
+function identity<Type>(arg: Type):Type{
+    return arg;
+}
+
+console.log(identity(25));
+console.log(identity('Hello'))
