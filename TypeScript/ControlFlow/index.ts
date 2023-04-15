46e52f31c2ee18d33e3eb0e78ee61dfdd3c7a3aa
unknown <asifs.cse@gmail.com>
2023-04-15 12:40:51 +0530

    control flow

---
+++ TypeScript/ControlFlow/index.ts
@@ -0,0 +1,16 @@
+
+
+//control flow
+
+let x=5;
+
+if(x==6){
+    console.log('yes');
+    if(x%2==0){
+        console.log('even');
+    }else{
+        console.log('odd');
+    }
+}else{
+    console.log('no');
+}
