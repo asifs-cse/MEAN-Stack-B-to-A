1d3a398a3df5555fb986cc11e6512aebcf8a0c7f
unknown <asifs.cse@gmail.com>
2023-04-19 23:27:11 +0530

    connect mongo db

--- MongoDB/Authentication/lession1/app.js
+++ MongoDB/Authentication/lession1/app.js
@@ -1,5 +1,50 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const PORT = 4000;
+const dbUrl = process.env.MONGO_URL;
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+mongoose.connect(dbUrl)
+.then(()=>{
+    console.log("mongo db is connected");
+})
+.catch((error)=>{
+    console.log(error);
+    process.exit(1);
+})
+
+//home route
+app.get("/",(req, res)=>{
+    res.sendFile("./index.html"  , {root: __dirname});
+});
+
+//register route
+app.get("/register",(req, res)=>{
+    const {email, password} = req.body;
+    res.status(201).json({email, password});
+});
+
+//register route
+app.get("/login",(req, res)=>{
+    res.status(201).json({message: "user is login"});
+});
+
+//rout not found
+app.use((req, res, next)=>{
+    res.status(404).json({
+        message: "route not found",
+    });
+});
+
+
+
+app.listen(process.env.PORT || PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
