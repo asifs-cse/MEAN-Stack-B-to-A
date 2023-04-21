b5561148993ae1500a470ac8371438a7ae1a15c0
unknown <asifs.cse@gmail.com>
2023-04-22 00:02:59 +0530

    add session

--- MongoDB/Project/User Register/index.js
+++ MongoDB/Project/User Register/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const session = require('express-session')
 const path = require('path');
 const PORT = 4040;
 const hbs = require('hbs');
@@ -7,10 +8,28 @@ const infoModel = require('./database');
 
 const templatePath = path.join(__dirname,'./templates/');
 
+app.use(session({secret:'measnstack'}));
+
 app.use(express.json());
 app.set('view engine', "hbs");
 app.set('views', templatePath);
 app.use(express.urlencoded({extended:false}));
+
+app.use(
+    session({
+        secret:'cse meanstack session example'
+    })
+)
+
+app.get('/count',(req, res)=>{
+    if(req.session.count){
+        req.session.count++;
+        res.send("You visited this page: "+req.session.count);
+    }else{
+        req.session.count =1;
+        res.send('first time');
+    }
+});
 
 app.get('/',(req, res)=>{
     res.render('login');
@@ -38,14 +57,29 @@ app.post('/signup', async (req,res)=>{
 app.post('/login', async (req,res)=>{
     try {
         const check = await infoModel.findOne({email:req.body.user_email});
-        if(check.password == req.body.user_password){
-            res.render("home");
+        if(check){
+            if(check.password == req.body.user_password){
+                res.render("home");
+                req.session.loggedin = true;
+            }else{
+                res.send('wrong password');
+            }
         }else{
-            res.send('wrong password');
+            res.send('User not registered')
         }
+        
     } catch (error) {
-        res.send('user not registerd');
+        res.send(error);
     }
+});
+
+app.get('/next',(req, res)=>{
+    if(req.session.loggedin){
+        res.send('Secreat content only for logged user');
+    }else{
+        res.send('Please login to view this page')
+    }
+    res.end();
 })
 
 app.listen(PORT, ()=>{
