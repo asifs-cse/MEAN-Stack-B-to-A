a5caddbb64a4f88a02c76b791f7c1f8f2c2d9357
unknown <asifs.cse@gmail.com>
2023-04-28 11:34:54 +0530

    export

---
+++ TypeScript/Baisc/MEAN Class/28.04.23/abstractClass.ts
@@ -0,0 +1,5 @@
+abstract class Demo123{
+    disp1():void{
+        console.log('welcome to acoe');
+    }
+}
---
+++ TypeScript/Baisc/MEAN Class/28.04.23/test2.ts
@@ -0,0 +1,7 @@
+import {x} from './test1';
+console.log(x);
+import {disp} from './test1';
+disp();
+import { cse } from './test1';
+var c = new cse(10,20);
+c.add();
